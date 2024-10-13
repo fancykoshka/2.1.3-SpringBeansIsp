6fbdaf87ade979c0c22425ba7c6d6956e26d3dd8
fancykoshka <150930511+fancykoshka@users.noreply.github.com>
2024-10-13 13:41:21 +0300

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/PP_2_1_3_SpringBeans-master/src/main/java/app/model/Dog.java
@@ -0,0 +1,10 @@
+package app.model;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+@Component
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Dog";
+    }
+}
